c8c5a4c0c5011f17f267325ed79d60860bf89ff3
Kinnto Lee <kicw118@gmail.com>
2023-12-08 18:45:19 -0500

    Add files via upload

---
+++ DisableShowMoreOptions.bat
@@ -0,0 +1,4 @@
+@echo off
+REG ADD "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f
+echo Windows Registry has been updated to disable "Show more options".
+pause
